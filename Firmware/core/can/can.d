c1f9f13773a4d9799c1f359411086f550c250456
Ostap Ferensovych <ostapvit@gmail.com>
2021-08-16 23:33:01 +0300

    Ignore .d .o
    Signed-off-by: Ostap Ferensovych <ostapvit@gmail.com>

--- Firmware/core/can/can.d
+++
@@ -1,7 +0,0 @@
-core/can/can.o: core/can/can.cpp core/can/can.hpp \
- core/capsense/capsense.hpp core/core/platform.hpp \
- cmsis/device/inc/stm32f0xx.h cmsis/device/inc/stm32f042x6.h \
- cmsis/inc/core_cm0.h cmsis/inc/cmsis_version.h \
- cmsis/inc/cmsis_compiler.h cmsis/inc/cmsis_gcc.h \
- cmsis/device/inc/system_stm32f0xx.h core/can/can_def.hpp \
- core/led/led.hpp
